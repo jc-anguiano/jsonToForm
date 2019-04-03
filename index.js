1c39c60c28ef064ebe07cb1200df24837120ddaa
Carlos Anguiano <carlos.anguiano27@gmail.com>
2019-04-03 02:46:42 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+exports.buildFrom = (jsonData, formRef) => {
+	jsonData.fields.map(field => {
+		let inputElement = document.createElement('input')
+		Object.keys(field.attrs).map(attrName => {
+			inputElement.setAttribute(attrName, field.attrs[attrName])
+		})
+		inputElement.setAttribute('type', field.type)
+		if(ref) formRef.appendChild(inputElement)
+	})
+}
